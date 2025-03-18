5566af295feb17fd6f07021a9847e8ce7a7e08b9
Guilherme S Beneti <guilhermesbeneti@MacBook-Air-de-Guilherme.local>
2025-03-18 15:21:18 -0300

    add kafka events

--- cmd/walletcore/main.go
+++ cmd/walletcore/main.go
@@ -7,13 +7,16 @@ import (
 
 	"github.com/GuilhermeBeneti1990/wallet-go/internal/database"
 	"github.com/GuilhermeBeneti1990/wallet-go/internal/event"
+	"github.com/GuilhermeBeneti1990/wallet-go/internal/event/handler"
 	createaccount "github.com/GuilhermeBeneti1990/wallet-go/internal/usecase/create_account"
 	createclient "github.com/GuilhermeBeneti1990/wallet-go/internal/usecase/create_client"
 	createtransaction "github.com/GuilhermeBeneti1990/wallet-go/internal/usecase/create_transaction"
 	"github.com/GuilhermeBeneti1990/wallet-go/internal/web"
 	"github.com/GuilhermeBeneti1990/wallet-go/internal/web/webserver"
 	"github.com/GuilhermeBeneti1990/wallet-go/pkg/events"
+	"github.com/GuilhermeBeneti1990/wallet-go/pkg/kafka"
 	"github.com/GuilhermeBeneti1990/wallet-go/pkg/uow"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -24,9 +27,18 @@ func main() {
 	}
 	defer db.Close()
 
+	configMap := ckafka.ConfigMap{
+		"bootstrap.servers": "kafka:29092",
+		"group.id":          "wallet",
+	}
+
+	kafkaProducer := kafka.NewKafkaProducer(&configMap)
+
 	eventDispatcher := events.NewEventDispatcher()
+	eventDispatcher.Register("TransactionCreated", handler.NewTransactionCreatedKafkaHandler(kafkaProducer))
+	eventDispatcher.Register("BalanceUpdated", handler.NewUpdateBalanceKafkaHandler(kafkaProducer))
 	transactionCreatedEvent := event.NewTransactionCreated()
-	// eventDispatcher.Register("TransactionCreated", handler)
+	balanceUpdatedEvent := event.NewBalanceUpdated()
 
 	clientDb := database.NewClientDB(db)
 	accountDb := database.NewAccountDB(db)
@@ -44,9 +56,9 @@ func main() {
 
 	createClientUseCase := createclient.NewCreateClientUseCase(clientDb)
 	createAccountUseCase := createaccount.NewCreateAccountUseCase(accountDb, clientDb)
-	createTransactionUseCase := createtransaction.NewCreateTransactionUseCase(uow, eventDispatcher, transactionCreatedEvent)
+	createTransactionUseCase := createtransaction.NewCreateTransactionUseCase(uow, eventDispatcher, transactionCreatedEvent, balanceUpdatedEvent)
 
-	webserver := webserver.NewWebServer(":3000")
+	webserver := webserver.NewWebServer(":8080")
 
 	clientHandler := web.NewWebClientHandler(*createClientUseCase)
 	accountHandler := web.NewWebAccountHandler(*createAccountUseCase)
@@ -56,5 +68,6 @@ func main() {
 	webserver.AddHandler("/accounts", accountHandler.CreateAccount)
 	webserver.AddHandler("/transactions", transactionHandler.CreateTransaction)
 
+	fmt.Println("Server is running")
 	webserver.Start()
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/GuilhermeBeneti1990/wallet-go
 go 1.23.5
 
 require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2
 	github.com/go-chi/chi/v5 v5.2.1
 	github.com/go-sql-driver/mysql v1.9.0
 	github.com/google/uuid v1.6.0
